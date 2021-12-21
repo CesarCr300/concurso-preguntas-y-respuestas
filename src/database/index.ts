a76e00c3d3c26ccc844d532ef2b7efeb09889625
CesarCr300 <cc30122003@gmail.com>
2021-12-21 16:20:58 -0500

    modelos creados

---
+++ src/database/index.ts
@@ -0,0 +1,5 @@
+import { db } from "./models/index";
+
+export async function connectDB(force:boolean=false){
+    await db.sequelize.sync({force});
+}
---
+++ src/database/seeders/Categorias.ts
@@ -0,0 +1,5 @@
+// import {db} from '../models/index';
+
+// const CategoriaModelo = db.Categoria;
+
+// function 
