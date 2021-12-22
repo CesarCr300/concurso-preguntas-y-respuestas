c6ca488a37cb510e472c32aabba5de177689a6ad
CesarCr300 <cc30122003@gmail.com>
2021-12-22 09:33:18 -0500

    implementacion seeder categorias

--- src/baseDeDatos/index.ts
+++ src/baseDeDatos/index.ts
@@ -1,7 +1,8 @@
 import { db } from "./models/index";
-
+import { seeder } from "./seeders/index";
 export async function connectDB(force: boolean = false) {
   await db.sequelize.sync({ force });
   if (force) {
+    seeder();
   }
 }
--- src/baseDeDatos/seeders/Categorias.ts
+++
@@ -1,8 +0,0 @@
-import {db} from '../models/index';
-
-const CategoriaModelo = db.Categoria;
-
-async function create(){
-    const categoria1 = await CategoriaModelo.create({nombre:"Matemática básica",nivel_dificultad:1,premio:"calculadora básica"})
-    await categoria1.addPregunta({pregunta:"¿Cual es el resultado de 3*3/2?", respuesta_correcta:"4.5"})
-}
